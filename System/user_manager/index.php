25a36fc4462085649d3f099e261255eb39c36cff
Aaron Williams <aharon.williams@gmail.com>
2023-02-10 02:07:10 -0600

    Update index.php

--- System/user_manager/index.php
+++ System/user_manager/index.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION['users'])) {
 }
 
 $action = filter_input(INPUT_POST, 'action');
+
 if ($action === NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action === NULL) {
