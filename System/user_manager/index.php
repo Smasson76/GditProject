15895c3ee72e497327121fde8b27cdbeb0c77935
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2023-02-10 00:20:52 -0600

    Created a login/signup page

--- System/user_manager/index.php
+++ System/user_manager/index.php
@@ -1,6 +1,15 @@
 <?php
-require('../model/database.php');
-require('../model/users_db.php');
 
+$action = filter_input(INPUT_POST, 'action');
+if ($action === NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action === NULL) {
+        $action = 'login_page';
+    }
+}
+
+if ($action == 'login_page') {
+    include('login_page.php');
+}
 
 ?>
