acecb8854445b0a33ec6926b170f785f44ff61e0
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2023-02-10 18:39:55 -0600

    Added fisma and fedramp information

--- System/user_manager/start_baseline.php
+++ System/user_manager/start_baseline.php
@@ -2,12 +2,26 @@
 session_start();
 include '../view/header.php'; ?>
 <main>
-    <nav> 
-    <ul>
-        <h2>Choose an ATO:</h2><br>
-        <li><a href="">FISMA</a></li>
-        <li><a href="">FedRAMP</a></li>
-    </ul>    
-    </nav>
+
+    <br><br>
+    <label for="ato"><h3>Choose an ATO:</h3></label>
+    <select name="ato" id="ato">
+    <optgroup label="FISMA">
+        <option value="blank"></option>
+        <option value="blank">NIST SP 800-16 - Training</option>
+        <option value="blank">NIST SP 800-18 R1 - Guide for developing security plans for federal systems</option>
+        <option value="blank">NIST SP 800-30 - Risk management guide for information technology systems</option>
+        <option value="blank">NIST SP 800-134 R1 - Contingency planning guide</option>
+        <option value="blank">NIST SP 800-53 Rev 4</option>
+    </optgroup>
+    <optgroup label="FedRAMP">
+        <option value="blank"></option>
+        <option value="blank">NIST SP 800-16 - Training</option>
+        <option value="blank">NIST SP 800-18 R1 - Guide for developing security plans for federal systems</option>
+        <option value="blank">NIST SP 800-30 - Risk management guide for information technology systems</option>
+        <option value="blank">NIST SP 800-134 R1 - Contingency planning guide</option>
+        <option value="blank">NIST SP 800-53 Rev 4</option>
+    </optgroup>
+</select>
 </section>
 <?php include '../view/footer.php'; ?>
