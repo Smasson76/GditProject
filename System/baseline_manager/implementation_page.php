a13aa23e898ef2e4fe57838747aa6ec049c079bc
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2023-03-02 17:19:48 -0600

    Added a button for the control selection page

---
+++ System/baseline_manager/implementation_page.php
@@ -0,0 +1,25 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+include '../view/header.php'; 
+include '../view/header-nav.php';
+
+?>
+
+<main>
+
+    <h2>Framework Control Selection</h2>
+
+    <table>
+        <tr>
+            <th>Control ID</th>
+            <th>Implementation Status</th>
+            <th>Control Organization</th>
+            <th>Comments</th>
+        </tr>
+        
+    </table>
+
+</section>
+<?php include '../view/footer.php'; ?>
